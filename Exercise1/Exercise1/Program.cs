a00c7ab17fae331389bb630459b4ce84d1b871f2
Lucas Mendes <lmendes@symplicity.com>
2023-05-11 19:28:29 -0300

    exercise1

---
+++ Exercise1/Exercise1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace Exercise1
+{
+    class Exercise1
+    {
+        static void Main(string[] args)
+        {
+            string produto1 = "Computador"; 
+            string produto2 = "Mesa de Escritório"; 
+
+            byte idade = 30; 
+            int codigo = 5290; 
+            char genero = 'M'; 
+
+            double preco1 = 2100.0; 
+            double preco2 = 650.50; 
+            double medida = 53.234567; 
+            
+            Console.WriteLine("Produtos: "); 
+            Console.WriteLine($"{produto1}, cujo preço é R${preco1:F2}"); 
+            Console.WriteLine($"{produto2}, cujo preço é R${preco2:F2}"); 
+            Console.WriteLine($"Registro: {idade} anos d idade, código {codigo} e gênero: {genero}"); 
+            Console.WriteLine($"Medida com 8 casas decimais: {medida:F8}"); Console.WriteLine($"Arredondado (3 casas decimais): {medida:F3}"); 
+            Console.WriteLine($"Separador decimal invariant culture: {medida.ToString("", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
